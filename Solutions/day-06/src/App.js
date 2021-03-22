35567d12e8308287d1653c59cb854f703a2ef9f4
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-03-22 10:13:59 +0100

    Refactor hexGenerator to class component

--- Solutions/day-06/src/App.js
+++ Solutions/day-06/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <h1>30 Days Of React</h1>
-      <App3 />
+      <App2 numbers = {numbers} />
     </div>
   );
 }
--- Solutions/day-06/src/day06/components/HexGenerator.js
+++ Solutions/day-06/src/day06/components/HexGenerator.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import Colours from './Colours';
 
-const HexGenerator = ({ numbers }) => {
-    const colorSquares = numbers.map(number => {
-        return <Colours key = {number} />
-    });
+class HexGenerator extends React.Component {
+    constructor(props) {
+        super(props)
+    }
 
-    return (
-        <div>
-            <h2>Hexadecimal Colors</h2>
-            <div className = "numbers-box">
-                {colorSquares}
+    render() {
+        const numbers = this.props.numbers;
+
+        const colorSquares = numbers.map(number => {
+            return <Colours key = {number} />
+        });
+
+        return (
+            <div>
+                <h2>Hexadecimal Colors</h2>
+                <div className = "numbers-box">
+                    {colorSquares}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
 
 export default HexGenerator;
